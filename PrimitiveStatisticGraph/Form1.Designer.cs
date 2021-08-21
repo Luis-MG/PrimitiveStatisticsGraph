704f7a50ea8c176eb66c3b5e3ae66839d09661f1
luismunozg <luifer@its.com.co>
2021-08-21 13:05:06 -0500

    Base Form

---
+++ PrimitiveStatisticGraph/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace PrimitiveStatisticGraph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.año = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.valueSales = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.año,
+            this.valueSales});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(246, 270);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // año
+            // 
+            this.año.HeaderText = "Año";
+            this.año.Name = "año";
+            // 
+            // valueSales
+            // 
+            this.valueSales.HeaderText = "Valor en Billones de USD";
+            this.valueSales.Name = "valueSales";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(306, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Actualizar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 326);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn año;
+        private System.Windows.Forms.DataGridViewTextBoxColumn valueSales;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PrimitiveStatisticGraph/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace PrimitiveStatisticGraph
+{    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+        //Trabajando con primitivas gráficas
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics lienzo = e.Graphics;
+            Pen lapiz = new Pen(Color.White, 2);
+            SolidBrush BrochaPinta = new SolidBrush(Color.White);
+            //===================================
+            //Rectángulo: Xpos, Ypos, Ancho, Alto
+            //===================================
+            lienzo.FillRectangle(BrochaPinta, 306, 12, 300, 270);
+
+            /*// Crea borcha.
+            SolidBrush redBrush = new SolidBrush(Color.Red);
+
+            //==============
+            //Letras
+            //==============
+            string Cadena = "Esta es una prueba";
+
+            //Fuente y la brocha con que se pinta.
+            Font FuenteLetra = new Font("Tahoma", 16);
+            SolidBrush BrochaPinta = new SolidBrush(Color.Black);
+
+            //Punto arriba a la izquierda para pintar la cadena
+            PointF PuntoCadena = new PointF(600.0F, 100.0F);
+
+            //Formato para dibujar
+            StringFormat FormatoDibuja = new StringFormat();
+            FormatoDibuja.FormatFlags = StringFormatFlags.DirectionVertical;
+
+            //Dibuja la cadena
+            lienzo.DrawString(Cadena, FuenteLetra, BrochaPinta, PuntoCadena, FormatoDibuja);
+
+
+            //================
+            //Dibuja un pastel
+            //================
+            //Crea un rectángulo para la elipse
+            Rectangle rectangulo = new Rectangle(300, 300, 100, 100);
+
+            //Pastel: rectángulo que contiene, ángulo inicial, ángulo abre
+            lienzo.FillPie(redBrush, rectangulo, 0F, 90F);
+            lienzo.DrawPie(lapiz, rectangulo, 0F, 90F);
+            lienzo.DrawPie(lapiz, rectangulo, 90F, 180F);
+            lienzo.DrawPie(lapiz, rectangulo, 180F, 270F);
+            lienzo.DrawPie(lapiz, rectangulo, 270F, 360F);*/
+        }
+        
+        private void drawPie(float[] pieProportion/*, object sender, PaintEventArgs e*/)
+        {
+            // Graphics lienzo = e.Graphics;
+            Graphics lienzo = this.CreateGraphics();
+            SolidBrush redBrush = new SolidBrush(Color.Red);
+            SolidBrush BrochaPinta = new SolidBrush(Color.White);
+            Rectangle rectangulo = new Rectangle(306, 12, 295, 265);
+            lienzo.FillPie(redBrush, rectangulo, 0F, 90F);
+            /* for( int i=0;i < pieProportion.Length; i++)
+            {
+                lienzo.FillPie(redBrush, rectangulo, 0F, 90F);
+                lienzo.FillRectangle(BrochaPinta, 306, 12, 300, 270);
+            }*/
+            
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            float[] pieProportion = new float[dataGridView1.RowCount];
+
+            int sum = 0;
+            for (int i = 0; i < dataGridView1.Rows.Count-1 ; ++i)
+            {
+                sum += int.Parse(dataGridView1.Rows[i].Cells["valueSales"].Value.ToString());
+            }
+            
+
+            for (int i = 0; i < dataGridView1.RowCount-1; i++)
+            {
+                pieProportion[i] = float.Parse(dataGridView1.Rows[i].Cells["valueSales"].Value.ToString()) / sum;
+            }
+            drawPie(pieProportion);
+        }
+    }
+}
